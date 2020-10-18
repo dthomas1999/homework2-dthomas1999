3d666611d1c1fddf4f9bc10f83b34e2c479974a3
dthom <dgthoma3@cougarnet.uh.edu>
2020-10-18 15:00:46 -0500

    Created Patient class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,6 +22,9 @@ public class Controller {
     @FXML
     private Button loadB;
 
+    @FXML
+    private ListView<Patient> patientsListView;
+
 //    @FXML
 //    private ListView<Patient> patientListView;
 
@@ -29,27 +34,34 @@ public class Controller {
         try {
             Connection conn = DriverManager.getConnection(url, "root", "password");
             Statement stmt = conn.createStatement();
+            try {
+                stmt.execute("CREATE TABLE test (" +
+                        "Name CHAR(25), " +
+                        "Age Int(3), " +
+                        "Gender CHAR(1), " +
+                        "BloodType CHAR(3), " +
+                        "Weight CHAR(5), " +
+                        "Height CHAR(4), " +
+                        "PatientID VARCHAR(36) )" );
 
-            stmt.execute("CREATE TABLE test (" +
-                    "Name CHAR(25), " +
-                    "Age Int(3), " +
-                    "Gender CHAR(1), " +
-                    "BloodType CHAR(3), " +
-                    "PatientID VARCHAR(36) )");
-
-            System.out.println("TABLE CREATED");
+                System.out.println("TABLE CREATED");
+            } catch (Exception ex) {
+                String msg = ex.getMessage();
+                System.out.println(msg);
+            }
 
             UUID id = UUID.randomUUID();
-            String patientID =  id.toString();
+            String patientID = id.toString();
             String patientName = "John Doe";
-            String  patientAge = "27";
+            String patientAge = "27";
             String gender = "M";
             String bloodType = "O";
+            String weight = "180";
+            String height = "5";
 
 
-            //add data
             String sql = "INSERT INTO test VALUES" +
-                    "('" + patientName + "', '" + patientAge + "', '" + gender + "', '" + bloodType + "', '" + patientID + "')";
+                    "('" + patientName + "', '" + patientAge + "', '" + gender + "', '" + bloodType + "', '" + weight + "', '" + height + "', '" + patientID + "')";
             stmt.executeUpdate(sql);
             System.out.println("TABLE FILLED");
 
@@ -60,10 +72,10 @@ public class Controller {
             String msg = ex.getMessage();
             System.out.println(msg);
         }
-
-
-
     }
+
+
+
 
     private void deleteTable(String url) {
         try {
@@ -97,6 +109,15 @@ public class Controller {
     }
 
     @FXML private void handleLoadAction(ActionEvent event) {
-        System.out.println("test");
+//        ObservableList<Patient> items = patientsListView.getItems();
+//        UUID id = UUID.randomUUID();
+//        String idString = id.toString();
+//        Patient Patient1 = new Patient(idString, "Jane Doe", 31, "F", "A", 156, "5:9" );
+//        Patient Patient2 = new Patient(idString, "Jhon Doe", 27, "M", "O", 180, "5:11");
+//        Patient Patient3 = new Patient(idString, "Kurt Ambrose", 51, "M", "B", 185, "6:1");
+//
+//        items.add(Patient1);
+//        items.add(Patient2);
+//        items.add(Patient3);
     }
 }
